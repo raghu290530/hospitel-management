906d8143548b2ab52c05b31b2fc428962e6503cd
raghu290530 <raghu290530@gmail.com>
2026-01-07 13:32:29 +0530

    first commit

--- js/include.js
+++ js/include.js
@@ -1,3 +1,7 @@
-
-fetch("components/header.html").then(r=>r.text()).then(d=>header.innerHTML=d);
-fetch("components/navbar.html").then(r=>r.text()).then(d=>navbar.innerHTML=d);
+fetch("components/header.html")
+  .then((r) => r.text())
+  .then((d) => (header.innerHTML = d));
+fetch("components/navbar.html")
+  .then((r) => r.text())
+  .then((d) => (navbar.innerHTML = d));
+  
